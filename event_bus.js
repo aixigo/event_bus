7dc11488442f5d2f7ce03ed7b6a5f0cdc06375a2
soffermann <steffen.offermann@aixigo.de>
2012-10-11 09:08:07 +0200

    Initial version

--- event_bus.js
+++ event_bus.js
@@ -13,16 +13,114 @@
 //  Aachen, Germany
 //
 define(
-   [ 'underscore' ],
+   [ 'underscore', './event' ],
 
-   function( _ )
+   ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   function( _, Event )
    {
       "use strict";
 
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      function EventBus()
+      {
+         EventBus.prototype.singleton_ = this;
+
+         this.queue_ = [];
+         this.subscribers = {};
+      }
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      EventBus.prototype.singleton()
+      {
+         if( _.isUndefined( EventBus.prototype.singleton_ ) ) {
+            EventBus.prototype.singleton_ = new EventBus();
+         }
+
+         return EventBus.prototype.singleton_;
+      }
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      EventBus.prototype.subscribe = function( subscriberId, callback )
+      {
+         subscribers[ subscriberId ] = { 'sendEvent': callback };
+      }
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      EventBus.prototype.unsubscribe = function( subscriberId )
+      {
+         delete this.subscribers[ subscriberId ];
+      }
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      EventBus.prototype.publish = function()
+      {
+         // Create a batch from the currently queued events as the queue might get modified during event
+         // processing
+         var batch = this.queue_;
+         this.queue_ = [];
+
+         // Send batch to all subscribers
+         _each( batch, function( event ){
+            _.each( this.subscribers, function( subscriber, subscriberId ) {
+               subscriber.sendEvent( event );
+            } );
+         } );
+      }
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      EventBus.prototype.pushEvent = function( name, subject, payload )
+      {
+         this.queue_.push( Event.create( name, subject, payload ) );
+      }
 
       ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
       return {
+         /**
+          * Adds an event to the event queue.
+          *
+          * @param name    The event name.
+          * @param subject The event subject.
+          * @param payload The event payload, if any.
+          */
+         'push': function( name, subject, payload ) {
+            EventBus.singleton().pushEvent( name, subject, payload );
+         },
+
+         /**
+          * Subscribes to this event bus. Subscribers will receive ALL events on the bus, i.e. they themselves
+          * are responsible for selecting the one they are interested in.
+          *
+          * @param subscriberId The subscriber's identifier.
+          * @param callback     The callback method. It takes an event as its only parameter.
+          */
+         'subscribe': function( subscriberId, callback ) {
+            EventBus.singleton().subscribe( subscriberId, callback );
+         },
+
+         /**
+          * Removes a subscriber's callback from the list of subscribers.
+          *
+          * @param subscriberId The subscriber's identifier.
+          */
+         'unsubscribe': function( subscriberId ) {
+            EventBus.singleton().unsubscribe( subscriberId );
+         },
+
+         /**
+          * Publishes all events currently on the bus (i.e. sends them to all subscribers) and removes them
+          * from the queue.
+          */
+         'publish': function() {
+            EventBus.singleton().publish();
+         }
       };
    }
 );

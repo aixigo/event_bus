c797a26f4cc749385172210a15c35954fefbc7a9
soffermann <steffen.offermann@aixigo.de>
2012-10-11 08:20:14 +0200

    Initial version

---
+++ event_bus.js
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2000-2012
+//    by aixigo AG, Aachen, Germany.
+//
+//  All rights reserved. This material is confidential and proprietary to AIXIGO AG and no part of this
+//  material should be reproduced, published in any form by any means, electronic or mechanical including
+//  photocopy or any information storage or retrieval system nor should the material be disclosed to third
+//  parties without the express written authorization of AIXIGO AG.
+//
+//  aixigo AG
+//  http://www.aixigo.de
+//  Aachen, Germany
+//
+define(
+   [ 'underscore' ],
+
+   function( _ )
+   {
+      "use strict";
+
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      return {
+      };
+   }
+);

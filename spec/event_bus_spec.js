e848e3aa67055f9fb60c4e0ef0b718ab2e012f7e
Alexander Wilden <alexander.wilden@aixigo.de>
2012-12-19 11:35:41 +0100

    Changed to MIT license for opensource release

--- spec/event_bus_spec.js
+++ spec/event_bus_spec.js
@@ -1,17 +1,24 @@
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//  Copyright (C) 2000-2012
-//    by aixigo AG, Aachen, Germany.
-//
-//  All rights reserved. This material is confidential and proprietary to AIXIGO AG and no part of this
-//  material should be reproduced, published in any form by any means, electronic or mechanical including
-//  photocopy or any information storage or retrieval system nor should the material be disclosed to third
-//  parties without the express written authorization of AIXIGO AG.
-//
-//  aixigo AG
-//  http://www.aixigo.de
-//  Aachen, Germany
-//
+/**
+ Copyright (c) 2012 aixigo AG <info@aixigo.de>
+ All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of
+ this software and associated documentation files (the 'Software'), to deal in
+ the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ the Software, and to permit persons to whom the Software is furnished to do so,
+ subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 define( [ 'event_bus' ], function( event_bus ) {
    'use strict';
 
